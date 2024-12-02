fed6cff134f805e75a40bab3331e1f15a62c8ed7
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-02 11:56:31 +0100

    create form info
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

---
+++ src/features/developer-background/ui/form-info.tsx
@@ -0,0 +1,9 @@
+export function FormInfo() {
+  return (
+    <form>
+      <input type="text" name="name" />
+      <input type="text" name="title" />
+      <input type="text" name="bio" />
+    </form>
+  )
+}
