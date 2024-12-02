da47b295b916f2af319eeb6e0dd49df4784311a6
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-12-02 16:12:08 +0100

    added functions to get commits, issues
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/projects/components/project-card.tsx
+++ src/features/projects/components/project-card.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ProjectDetails from "./project-details";
 import { projectService } from "../instance";
+import { getTotalOfCommits } from "../api/api";
 
 export async function ProjectCard() {
   const projects = await projectService.getAll();
+  const data = await getTotalOfCommits();
+  console.log({ data });
   return (
     <div>
       <h3>Projects</h3>
