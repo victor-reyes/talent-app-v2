e0102bd95b5fefe5a1580d8d593da3277be73b08
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-02 14:31:48 +0100

    change the primary color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import ProjectCard from "@/features/projects/components/project-card";
 
 export default function Home() {
-  return <ProjectCard />;
+  return (
+  <>
+  <Button className="p-5">Hello</Button>
+  <ProjectCard />
+  </>
+  );
 }
