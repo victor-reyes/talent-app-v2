22457e21c206dc0be7a04e258bd5164c73a244ff
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-02 15:07:46 +0100

    create card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-import ProjectCard from "@/features/projects/components/project-card";
+import { DeveloperCard } from "@/features/design-system/developer-card";
 
 export default function Home() {
   return (
   <>
-  <Button className="p-5">Hello</Button>
-  <ProjectCard />
+  <DeveloperCard />
+
+
   </>
   );
 }
---
+++ src/features/design-system/developer-card.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export function DeveloperCard() {
+  return (
+    <section className="flex justify-center items-center">
+      <Card className="p-4">
+        <CardContent className="">
+          <p>Developer</p>
+        </CardContent>
+      </Card>
+    </section>
+  );
+}
