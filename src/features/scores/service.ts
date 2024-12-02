8576ff029b6f5121351c19c7ca4d792f345f61c3
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-02 15:07:14 +0100

    added-test-user
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/service.ts
+++ src/features/scores/service.ts
@@ -1,3 +1,14 @@
+
+const firstUser = {
+  frontend: 67,
+  backend: 72,
+  charismatic: 70,
+  teamwork: 50,
+  design: 100,
+  management: 87
+}
+
+
 export const createService = () => {
   return {
     kada: "Says hello",
