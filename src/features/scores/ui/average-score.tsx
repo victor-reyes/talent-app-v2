02ab61d5ac18310fdeea81ea6d30c93a1cc3f8a6
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-03 10:42:34 +0100

    made-progress-ring-with-dynamic-%
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/ui/average-score.tsx
+++ src/features/scores/ui/average-score.tsx
@@ -1,8 +1,36 @@
+import React from "react";
+
 export function AverageScore() {
+  const percentage = 22;
+
   return (
     <div className="flex justify-start items-start">
       <div className="flex border-2 rounded-full py-1 px-2 items-center border-purple-500">
-        <div className="flex justify-center items-center border-4 rounded-full h-10 w-10 text-xs border-green-400">100%</div>
+        <div className="relative flex justify-center items-center h-16 w-16">
+          <svg className="absolute" viewBox="0 0 36 36">
+            <path
+              className="circle-bg"
+              d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+              fill="none"
+              stroke="#e6e6e6"
+              strokeWidth="2"
+            />
+            <path
+              className="circle"
+              d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+              fill="none"
+              stroke="#4caf50"
+              strokeWidth="2"
+              strokeDasharray={`${percentage}, 100`}
+              strokeLinecap="round"
+            />
+          </svg>
+          <div className="absolute text-xs font-bold">{`${percentage}%`}</div>
+        </div>
         <div className="font-semibold px-6">Level 3</div>
       </div>
     </div>
