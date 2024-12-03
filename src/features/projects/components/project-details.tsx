c9d515d874fb25e28f71e5ea2234af17301d02b7
CarlUdo <carl@wbsweden.com>
2024-12-03 16:28:02 +0100

    Remove-card-component
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/projects/components/project-details.tsx
+++ src/features/projects/components/project-details.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FaCodeCommit, FaGithub } from "react-icons/fa6";
 import { HeaderH3 } from "@/components/ui/header/header-h3";
 import Link from "next/link";
@@ -25,57 +24,51 @@ export default function ProjectDetails({ project }: Props) {
   const { title, link, commits, issues, duration, performance, tests } =
     project;
   return (
-      <Card>
-        <CardHeader className="mb-0 pb-0">
-          <CardTitle className="pb-2">
-            <div className="flex  gap-4">
-              <HeaderH3>{title}</HeaderH3>
-              <Link
-                href={link}
-                className="mb-8 font-medium text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                <FaGithub size={26} color="gray" />
-              </Link>
-            </div>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <section className="flex flex-col justify-center items-center">
-            <article className="flex flex-col items-center">
-              <GiDuration size={28} color="gray" />
-              <p className="text-gray-400 text-lg">Duration</p>
-              <p className="font-semibold text-xl">{duration}</p>
-            </article>
-            <Separator className="my-4" />
-            <div className="flex justify-between gap-6">
-              <div className="flex items-center justify-center gap-2">
-                <article className="flex flex-col items-center">
-                  <FaCodeCommit />
-                  <p className="text-gray-400 text-sm">Commits</p>
-                  <p className="font-semibold">{commits}</p>
-                </article>
-                <Separator orientation="vertical" />
-                <article className="flex flex-col items-center">
-                  <GoIssueOpened />
-                  <p className="text-gray-400 text-sm">Issues</p>
-                  <p className="font-semibold">{issues}</p>
-                </article>
-                <Separator orientation="vertical" />
-                <article className="flex flex-col items-center">
-                  <CgPerformance />
-                  <p className="text-gray-400 text-sm">Performance</p>
-                  <p className="font-semibold">{performance}%</p>
-                <Separator orientation="vertical" />
-                </article>
-                <article className="flex flex-col items-center">
-                  <SiTestcafe />
-                  <p className="text-gray-400 text-sm">Tests</p>
-                  <p className="font-semibold">{tests}</p>
-                </article>
-              </div>
-            </div>
-          </section>
-        </CardContent>
-      </Card>
+    <>
+    <div className="flex gap-4">
+      <HeaderH3>{title}</HeaderH3>
+      <Link
+        href={link}
+        className="mb-8 font-medium text-blue-600 dark:text-blue-500 hover:underline"
+      >
+        <FaGithub size={26} color="gray" />
+      </Link>
+  </div>
+  <section className="flex flex-col justify-center items-center">
+  <article className="flex flex-col items-center mb-4">
+    <GiDuration size={28} color="gray" />
+    <p className="text-gray-400 text-lg">Duration</p>
+    <p className="font-semibold text-xl">{duration}</p>
+  </article>
+
+  <div className="flex justify-between gap-6">
+    <div className="flex items-center justify-center gap-4">
+      <article className="flex flex-col items-center">
+        <FaCodeCommit />
+        <p className="text-gray-400 text-sm">Commits</p>
+        <p className="font-semibold">{commits}</p>
+      </article>
+      <Separator orientation="vertical" />
+      <article className="flex flex-col items-center">
+        <GoIssueOpened />
+        <p className="text-gray-400 text-sm">Issues</p>
+        <p className="font-semibold">{issues}</p>
+      </article>
+      <Separator orientation="vertical" />
+      <article className="flex flex-col items-center">
+        <CgPerformance />
+        <p className="text-gray-400 text-sm">Performance</p>
+        <p className="font-semibold">{performance}%</p>
+      </article>
+      <Separator orientation="vertical" />
+      <article className="flex flex-col items-center">
+        <SiTestcafe />
+        <p className="text-gray-400 text-sm">Tests</p>
+        <p className="font-semibold">{tests}</p>
+      </article>
+    </div>
+  </div>
+</section>
+</>
   );
 }
