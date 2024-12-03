a228bf1d15be1b6d9e2383ff8031adad9e0ba3d8
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-03 11:49:48 +0100

    Rename shortBio into bio

--- src/features/developer-background/ui/developer-profile.tsx
+++ src/features/developer-background/ui/developer-profile.tsx
@@ -29,7 +29,7 @@ export function DeveloperProfile() {
 export function BasicInfo(user: {
   name: string;
   title: string;
-  shortBio: string;
+  bio: string;
 }) {
   return (
     <div className="flex gap-4">
@@ -41,7 +41,7 @@ export function BasicInfo(user: {
       <div>
         <h2>{user.title}</h2>
         <h2>{user.name}</h2>
-        <h2>{user.shortBio}</h2>
+        <h2>{user.bio}</h2>
       </div>
       <SocialLink url={"https://github.com"} name={"Github"} />
     </div>
