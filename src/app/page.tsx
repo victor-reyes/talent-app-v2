477938f62c86d34fc91d1df822f17828dccac451
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-03 13:33:30 +0100

    add components to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { DeveloperCard } from "@/components/ui/developer-card";
 import { ProjectCard } from "@/features/projects/components/project-card";
+import { ScoreBoard } from "@/features/scores";
 
 export default function Home() {
   return (
     <>
-      <DeveloperCard>hi</DeveloperCard>
+      <DeveloperCard>
+       <ScoreBoard/>
       <ProjectCard />
+      </DeveloperCard>
     </>
   );
 }
