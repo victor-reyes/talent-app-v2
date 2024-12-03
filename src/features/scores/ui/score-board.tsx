ab7920b64e1aeb16a7ab49a7c6691e978afc14da
CarlUdo <carl@wbsweden.com>
2024-12-03 15:59:02 +0100

    Change-heading
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/ui/score-board.tsx
+++ src/features/scores/ui/score-board.tsx
@@ -1,15 +1,17 @@
 
-import { HeaderH1 } from "@/components/ui/header/header-h1";
+import { Separator } from "@/components/ui/separator";
 import { AccordionBoard } from "./accordion/accordion-board";
 import { AverageScore } from "./average-score";
 import { SpiderGraph } from "./spider-graph";
+import { HeaderH2 } from "@/components/ui/header/header-h2";
 
 export function ScoreBoard() {
   return (
     <section>
-      <HeaderH1>
+      <Separator className="my-4" />
+      <HeaderH2>
         Salt Scoring
-        </HeaderH1>
+        </HeaderH2>
       <AverageScore />
       <SpiderGraph />
       <AccordionBoard />
