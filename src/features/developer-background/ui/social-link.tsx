e987a4064d1763c656de5ea02a641f19c77112ae
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-03 12:44:57 +0100

    Add size prop to SocialLink button

--- src/features/developer-background/ui/social-link.tsx
+++ src/features/developer-background/ui/social-link.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
-type Props = { url: string; name: "Github" | "LinkedIn" };
-export function SocialLink({ url, name }: Props) {
+type Props = { url: string; name: "Github" | "LinkedIn", size?: number};
+export function SocialLink({ url, name, size = 24 }: Props) {
   return (
     <>
       {name === "Github" && (
         <Link href={url}>
-          <FaGithub />
+          <FaGithub size={size} />
         </Link>
       )}
       {name === "LinkedIn" && (
         <Link href={url}>
-          <FaLinkedin />
+          <FaLinkedin size={size} />
         </Link>
       )}
     </>
