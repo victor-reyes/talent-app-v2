6a0a393a862ac9c54b80e44817e269b09a467bf3
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-03 10:26:55 +0100

    installed avatar component from shadcn and used it
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/developer-background/ui/developer-profile.tsx
+++ src/features/developer-background/ui/developer-profile.tsx
@@ -1,6 +1,7 @@
 import { CardContent } from "@/components/ui/card";
 import { Skills } from "./skills";
 import { Row } from "./row";
+import { DeveloperAvatar } from "./developer-avatar";
 
 export function DeveloperProfile() {
   return (
@@ -10,9 +11,15 @@ export function DeveloperProfile() {
         title={"Javascript Developer"}
         shortBio={"Stuff"}
       />
-      
-      <Row title="Languages" content={["English", "Bulgarian", "Swedish", "Portuguese"]}/>
-      <Row title="Education" content={["BSc Computer Science", "Phd Bulgarian"]}/>
+
+      <Row
+        title="Languages"
+        content={["English", "Bulgarian", "Swedish", "Portuguese"]}
+      />
+      <Row
+        title="Education"
+        content={["BSc Computer Science", "Phd Bulgarian"]}
+      />
       <Skills skills={["Javascript", "React", "Node"]} />
     </CardContent>
   );
@@ -24,10 +31,17 @@ export function BasicInfo(user: {
   shortBio: string;
 }) {
   return (
-    <>
-      <h2>{user.title}</h2>
-      <h2>{user.name}</h2>
-      <h2>{user.shortBio}</h2>
-    </>
+    <div className="flex gap-4">
+      <DeveloperAvatar
+        url={
+          "https://variety.com/wp-content/uploads/2021/04/Avatar.jpg?w=800&h=533&crop=1"
+        }
+      />
+      <div>
+        <h2>{user.title}</h2>
+        <h2>{user.name}</h2>
+        <h2>{user.shortBio}</h2>
+      </div>
+    </div>
   );
 }
