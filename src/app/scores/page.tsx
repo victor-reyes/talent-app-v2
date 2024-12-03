fb362a7481233fca472a183031aa6e21f7a62d37
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-03 09:59:35 +0100

    made-score-page
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ src/app/scores/page.tsx
@@ -0,0 +1,14 @@
+import { DeveloperCard } from "@/components/ui/developer-card";
+import { HeaderH1 } from "@/components/ui/header/header-h1";
+
+
+
+export default function Page(){
+    return (
+    <DeveloperCard>
+        <HeaderH1>
+            Hej!
+        </HeaderH1>
+    {}
+    </DeveloperCard >
+)}
