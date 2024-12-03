6ba225179943b7b966b44fceddef0c5111181549
CarlUdo <carl@wbsweden.com>
2024-12-03 16:29:39 +0100

    Make-separation-between-dev
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/projects/components/project-card.tsx
+++ src/features/projects/components/project-card.tsx
@@ -3,17 +3,21 @@ import ProjectDetails from "./project-details";
 import { projectService } from "../instance";
 // import { getTotalOfCommits } from "../api/api";
 import { HeaderH2 } from "@/components/ui/header/header-h2";
+import { Separator } from "@/components/ui/separator";
 
 export async function ProjectCard() {
   const projects = await projectService.getAll();
   // const data = await getTotalOfCommits();
   // console.log({ data });
   return (
-    <div>
+    <div className="mt-4">
       <HeaderH2>Projects</HeaderH2>
-      <div className="flex flex-col items-center justify-center gap-4 mt-4">
+      <div className="flex flex-col justify-center mt-4">
       {projects.map((project, index) => (
-        <ProjectDetails key={index} project={project} />
+        <div key={index}>
+        <ProjectDetails project={project} />
+        <Separator className="my-4" />
+        </div>
       ))}
       </div>
     </div>
