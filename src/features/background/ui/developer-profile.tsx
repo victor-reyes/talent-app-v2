086755b0d636ad6b657bbd08cca01f2ba0a3c432
Odin Thif <mjd.thif@icloud.com>
2024-12-03 15:14:43 +0100

    using modal to edit developer info
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/background/ui/developer-profile.tsx
+++ src/features/background/ui/developer-profile.tsx
@@ -2,7 +2,7 @@ import { Skills } from "./skills";
 import { Row } from "./row";
 import { SocialLink } from "./social-link";
 import { BasicInfo } from "./basic-info";
-import { EditInfoForm } from "./form-info";
+// import { EditInfoForm } from "./form-info";
 
 type Props = {
   developer?: {
@@ -22,8 +22,7 @@ type Props = {
 
 export function Background({ developer = mockDeveloper }: Props) {
   return (
-    <div className="space-y-4">
-      {/* <FormInfo /> */}
+    <div className="space-y-2 max-w-96">
       <BasicInfo
         name={developer.name}
         title={developer.title}
