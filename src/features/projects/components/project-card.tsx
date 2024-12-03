0ef6a7c835eabfba9942830b5ffb38ff5bd91904
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-03 12:26:50 +0100

    style projects card

--- src/features/projects/components/project-card.tsx
+++ src/features/projects/components/project-card.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import ProjectDetails from "./project-details";
 import { projectService } from "../instance";
 import { getTotalOfCommits } from "../api/api";
+import { HeaderH2 } from "@/components/ui/header/header-h2";
 
 export async function ProjectCard() {
   const projects = await projectService.getAll();
   const data = await getTotalOfCommits();
   console.log({ data });
   return (
-    <div>
-      <h3>Projects</h3>
+    <div className="w-screen">
+      <HeaderH2>Projects</HeaderH2>
+      <div className="flex flex-col items-center justify-center gap-4 mt-4">
       {projects.map((project, index) => (
         <ProjectDetails key={index} project={project} />
       ))}
+      </div>
     </div>
   );
 }
