0f66c12cdc896dff2b34592fd273889ff25d3b2b
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-03 12:03:35 +0100

    prettier
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/ui/spider-graph.tsx
+++ src/features/scores/ui/spider-graph.tsx
@@ -1,16 +1,20 @@
-"use client"
-
-import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from "recharts"
+"use client";
 
 import {
-  CardContent,
-} from "@/components/ui/card"
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+} from "recharts";
+
+import { CardContent } from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
 
 const chartData = [
   { category: "Frontend", score: 94 },
@@ -19,22 +23,19 @@ const chartData = [
   { category: "Teamwork", score: 80 },
   { category: "Design", score: 72 },
   { category: "Management", score: 80 },
-]
+];
 
 const chartConfig = {
   desktop: {
     label: "Desktop",
     color: "hsl(var(--chart-1))",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 export function SpiderGraph() {
   return (
     <CardContent className="pb-0">
-      <ChartContainer
-        config={chartConfig}
-        className="mx-auto max-h-[250px]"
-      >
+      <ChartContainer config={chartConfig} className="mx-auto max-h-[250px]">
         <RadarChart data={chartData} outerRadius="80%">
           <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
           <PolarGrid />
@@ -49,5 +50,5 @@ export function SpiderGraph() {
         </RadarChart>
       </ChartContainer>
     </CardContent>
-  )
+  );
 }
