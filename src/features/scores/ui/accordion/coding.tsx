fff153a6577778296fcbd8eb5814f4ddb01b77ee
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-03 14:09:29 +0100

    added-codning-accordion
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/ui/accordion/coding.tsx
+++ src/features/scores/ui/accordion/coding.tsx
@@ -1,3 +1,23 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { ProgressRing } from "../progress-ring"
+
 export function Coding() {
-  return <div>Coding</div>;
+  return (
+    <Accordion type="single" collapsible className="mx-auto max-w-96">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>
+        <ProgressRing percentage={100}/>
+        Coding
+        </AccordionTrigger>
+        <AccordionContent>
+          Coding data
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
 }
