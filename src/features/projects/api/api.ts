91cab6be8bf2be7a25a011e229fd880f3e876dde
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-03 15:09:20 +0100

    new params for api-calls

--- src/features/projects/api/api.ts
+++ src/features/projects/api/api.ts
@@ -1,26 +1,23 @@
-import * as chromeLauncher from "chrome-launcher";
-import lighthouse, { Flags } from "lighthouse";
-
 //Make this generic later on, for now it's just a simple fetch
-export async function getTotalOfCommits() {
+export async function getTotalOfCommits(user: string, repo: string) {
   const response = await fetch(
-    "https://api.github.com/repos/alimohseni99/portfolio/stats/participation"
+    `https://api.github.com/repos/${user}/${repo}/stats/participation`
   );
   const data = await response.json();
   data.total = data.all.reduce((acc: number, value: number) => acc + value, 0);
   return data.total;
 }
 
-export async function getAllIssues() {
+export async function getAllIssues(user: string, repo: string) {
   const response = await fetch(
-    "https://api.github.com/repos/alimohseni99/portfolio/issues"
+    `https://api.github.com/repos/${user}/${repo}/issues`
   );
   const data = await response.json();
   return data;
 }
-export async function getDuration() {
+export async function getDuration(user: string, repo: string) {
   const response = await fetch(
-    "https://api.github.com/repos/alimohseni99/portfolio/commits"
+    `https://api.github.com/repos/${user}/${repo}/commits`
   );
   const data = await response.json();
   const firstCommit = data[data.length - 1].commit.author.date;
