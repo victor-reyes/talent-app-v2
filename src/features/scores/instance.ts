fe0805af2ff22af19c15def1d34bb6378c496285
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-04 14:08:10 +0100

    add-math-random-and-id
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/instance.ts
+++ src/features/scores/instance.ts
@@ -1,6 +1,7 @@
 import { createService } from "./service";
 
 const user = {
+    id: 1,
     frontend: Math.floor((Math.random() * 101)),
     backend: Math.floor((Math.random() * 101)),
     individualCommunication: Math.floor((Math.random() * 101)),
