f0665b9168e469aca306a9f1f7eff51580623b25
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-04 14:08:10 +0100

    add-math-random-and-id
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/service.ts
+++ src/features/scores/service.ts
@@ -1,24 +1,27 @@
 import { User } from "./types";
 
 export const createService = (user: User) => {
+
+
+
   return {
     getDeveloperScoreById: async () => {
       const programmingScore = (user.frontend + user.backend) / 2;
       const planningScore = (user.management + user.design) / 2;
       const communicationScore = (user.individualCommunication + user.teamCollaboration) / 2;
-      const averageScore = (programmingScore + planningScore + communicationScore) / 3;
+      const averageScore = Math.round((programmingScore + planningScore + communicationScore) / 3);
 
       return {
-        frontend: user.frontend,
-        backend: user.backend,
-        individualCommunication: user.individualCommunication,
-        teamCollaboration: user.teamCollaboration,
-        design: user.design,
-        management: user.management,
-        programmingScore,
-        planningScore,
-        communicationScore,
-        averageScore
+        frontend: Math.round(user.frontend),
+        backend: Math.round(user.backend),
+        individualCommunication: Math.round(user.individualCommunication),
+        teamCollaboration: Math.round(user.teamCollaboration),
+        design: Math.round(user.design),
+        management: Math.round(user.management),
+        programmingScore: Math.round(programmingScore),
+        planningScore: Math.round(planningScore),
+        communicationScore: Math.round(communicationScore),
+        averageScore,
       }
     },
   }
