86fc60496dca33b90d79aee977f7d40cdf3be350
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-04 16:24:58 +0100

    changed the graph color
    Co-authored-by: Carl <carl@wbsweden.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/scores/ui/spider-graph.tsx
+++ src/features/scores/ui/spider-graph.tsx
@@ -56,7 +56,7 @@ export function SpiderGraph({ developerScore }: Props) {
           <Radar
             dataKey="score"
             stroke="#a9a9a9"
-            fill="#a9a9a9"
+            fill="#6b7280"
             fillOpacity={0.6}
           />
         </RadarChart>
