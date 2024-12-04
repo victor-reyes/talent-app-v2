2d3dd2d1d6e6def033dea680de844e1d0d515839
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-04 15:10:03 +0100

    made-lvl-dynemic
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/ui/level-rectangle.tsx
+++ src/features/scores/ui/level-rectangle.tsx
@@ -18,10 +18,10 @@ export function LevelRectangle({ percentage }: Props) {
 
   const getLevel = (percentage: number) => {
     if (percentage === 0) return 0;
-    if (percentage <= 20) return 1;
-    if (percentage <= 40) return 2;
-    if (percentage <= 60) return 3;
-    if (percentage <= 80) return 4;
+    if (percentage <= 50) return 1;
+    if (percentage <= 69) return 2;
+    if (percentage <= 79) return 3;
+    if (percentage <= 89) return 4;
     return 5;
   };
 
