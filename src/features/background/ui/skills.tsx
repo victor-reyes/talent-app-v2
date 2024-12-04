92da96384f901cf6183ac90c3ea6a95aa1210596
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-04 16:07:32 +0100

    skills can be undefined and they are shown if they are not undefined
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/background/ui/skills.tsx
+++ src/features/background/ui/skills.tsx
@@ -4,16 +4,17 @@ import { Badge } from "@/components/ui/badge";
 type Props = { skills: string[] | undefined };
 export function Skills({ skills }: Props) {
   return (
-    <article className="flex justify-end flex-wrap gap-1 mt-3">
-      {skills.map((skill) => (
-        <Badge
-          className="cursor-pointer"
-          key={skill}
-          onClick={() => console.log(skill)}
-        >
-          {skill}
-        </Badge>
-      ))}
+    <article className="flex justify-start flex-wrap gap-1 mt-3">
+      {skills &&
+        skills.map((skill) => (
+          <Badge
+            className="cursor-pointer"
+            key={skill}
+            onClick={() => console.log(skill)}
+          >
+            {skill}
+          </Badge>
+        ))}
     </article>
   );
 }
