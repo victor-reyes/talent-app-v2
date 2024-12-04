b08615e5abffc076a519a74c314e2addd64539d8
CarlUdo <carl@wbsweden.com>
2024-12-04 13:23:12 +0100

    Make-getDeveloperScoreById-service-function
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/service.ts
+++ src/features/scores/service.ts
@@ -1,16 +1,42 @@
 
-const firstUser = {
+const user = {
   frontend: 67,
   backend: 72,
-  charismatic: 70,
-  teamwork: 50,
+  individualCommunication: 70,
+  teamCollaboration: 50,
   design: 100,
   management: 87
 }
 
+type User = {
+  frontend: number;
+  backend: number;
+  individualCommunication: number;
+  teamCollaboration: number;
+  design: number;
+  management: number;
+}
 
-export const createService = () => {
+export const createService = (user: User) => {
   return {
-    kada: "Says hello",
+    getDeveloperScoreById: async () => {
+      const programmingScore = (user.frontend + user.backend) / 2;
+      const planningScore = (user.management + user.design) / 2;
+      const communicationScore = (user.individualCommunication + user.teamCollaboration) / 2;
+      const averageScore = (programmingScore + planningScore + communicationScore) / 3;
+
+      return {
+        frontend: user.frontend,
+        backend: user.backend,
+        individualCommunication: user.individualCommunication,
+        teamCollaboration: user.teamCollaboration,
+        design: user.design,
+        management: user.management,
+        programmingScore,
+        planningScore,
+        communicationScore,
+        averageScore
+      }
+    },
   }
 }
