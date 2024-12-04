a3b60d8081719ec8dc97f757a0f74d74c8ba0b38
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-04 16:51:20 +0100

    scroll down when opening the new project form

--- src/features/projects/components/add-project.tsx
+++ src/features/projects/components/add-project.tsx
@@ -1,17 +1,28 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProjectForm from "./project-form";
 import { Button } from "@/components/ui/button";
 
 export default function AddProject() {
   const [showForm, setShowForm] = useState(false);
+  const formRef = useRef<HTMLDivElement>(null);
   function onClick() {
     setShowForm(!showForm);
   }
+
+  useEffect(() => {
+    if (showForm && formRef.current) {
+      formRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [showForm]);
   return (
     <div>
       <Button onClick={onClick}>{showForm ? "Cancel" : "Add project"}</Button>
-      {showForm ? <ProjectForm /> : undefined}
+      {showForm && (
+        <div ref={formRef}>
+          <ProjectForm />
+        </div>
+      )}
     </div>
   );
 }
