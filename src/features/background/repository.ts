d373d05bda4599b5fccbc0c6fa3881eb17ea68e7
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-04 16:03:40 +0100

    added mochbackground type and mockdata in repository
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/background/repository.ts
+++ src/features/background/repository.ts
@@ -2,10 +2,61 @@ import { db } from "@/db";
 import { eq } from "drizzle-orm";
 import { backgroundsTable, BackgroundInsert, BackgroundSelect } from "./schema";
 
+type Link = {
+  name: "Github" | "LinkedIn";
+  url: string;
+};
+
+export type MockBackground = {
+  id: number;
+  name: string;
+  title: string;
+  bio: string;
+  avatarURL: string;
+  languages?: string[] | undefined;
+  educations?: string[] | undefined;
+  skills?: string[];
+  links?: Link[] | undefined;
+};
+
+const mockBackgrounds: MockBackground[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    title: "Software Engineer",
+    bio: "I'm a software engineer with 5 years of experience",
+    avatarURL: "https://avatars.githubusercontent.com/u/1?v=4",
+    languages: ["JavaScript", "TypeScript", "Python"],
+    educations: ["BSc in Computer Science"],
+    skills: [
+      "React",
+      "Next.js",
+      "Node.js",
+      "Django",
+      "Tailwind CSS",
+      "PostgreSQL",
+    ],
+    links: [
+      {
+        name: "Github" as "Github" | "LinkedIn",
+        url: "https://github.com/johndoe",
+      },
+      {
+        name: "LinkedIn" as "Github" | "LinkedIn",
+        url: "https://linkedin.com/johndoe",
+      },
+    ],
+  },
+];
+
 export function createRepository() {
   return {
     async get() {
-      return db.select().from(backgroundsTable);
+      // return db.select().from(backgroundsTable);
+      // return mockBackground;
+    },
+    async getById(id: number) {
+      return mockBackgrounds[id];
     },
     async add(background: BackgroundInsert) {
       return db.insert(backgroundsTable).values({ ...background });
