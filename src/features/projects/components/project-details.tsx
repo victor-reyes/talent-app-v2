a92f31c28f2d25514acd2511c0436a11fd785aae
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-04 11:42:55 +0100

    change metrix layout

--- src/features/projects/components/project-details.tsx
+++ src/features/projects/components/project-details.tsx
@@ -24,47 +24,44 @@ export default async function ProjectDetails({ project }: Props) {
   const { username, repository } = project;
   return (
     <>
-      <Link href={`https://github.com/${username}/${repository}`} className="flex gap-2">
+      <Link
+        href={`https://github.com/${username}/${repository}`}
+        className="flex gap-2"
+      >
         <HeaderH3>{repository}</HeaderH3>
         <FaGithub size={26} color="gray" />
       </Link>
-        <section className="flex flex-col justify-center items-center mt-6">
-          <article className="flex flex-col items-center mb-4">
-            <GiDuration size={28} color="gray" />
-            <p className="text-gray-400 text-lg">Duration</p>
-            <p className="font-semibold text-xl">
-              {repositoryInformation.duration}
-            </p>
-          </article>
+      <section className="flex justify-between items-center mt-6">
+        <article className="flex flex-col items-center mb-4">
+          <GiDuration size={28} color="gray" />
+          <p className="text-gray-400 text-lg">Duration</p>
+          <p className="font-semibold text-xl">
+            {repositoryInformation.duration}
+          </p>
+        </article>
 
-          <div className="flex justify-between gap-6">
-            <div className="flex items-center justify-center gap-4">
-              <article className="flex flex-col items-center">
-                <FaCodeCommit />
-                <p className="text-gray-400 text-sm">Commits</p>
-                <p className="font-semibold">{repositoryInformation.commits}</p>
-              </article>
-              <Separator orientation="vertical" />
-              <article className="flex flex-col items-center">
-                <GoIssueOpened />
-                <p className="text-gray-400 text-sm">Issues</p>
-                <p className="font-semibold">{repositoryInformation.issues}</p>
-              </article>
-              <Separator orientation="vertical" />
-              <article className="flex flex-col items-center">
-                <CgPerformance />
-                <p className="text-gray-400 text-sm">Performance</p>
-                <p className="font-semibold">56%</p>
-              </article>
-              <Separator orientation="vertical" />
-              <article className="flex flex-col items-center">
-                <SiTestcafe />
-                <p className="text-gray-400 text-sm">Tests</p>
-                <p className="font-semibold">10</p>
-              </article>
-            </div>
+        <div className="flex justify-between gap-6">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <article className="flex flex-col items-center">
+              <FaCodeCommit />
+              <p className="text-gray-400 text-sm">Commits</p>
+              <p className="font-semibold">{repositoryInformation.commits}</p>
+            </article>
+            <Separator orientation="horizontal" />
+            <article className="flex flex-col items-center">
+              <GoIssueOpened />
+              <p className="text-gray-400 text-sm">Issues</p>
+              <p className="font-semibold">{repositoryInformation.issues}</p>
+            </article>
+            <Separator orientation="horizontal" />
+            <article className="flex flex-col items-center">
+              <CgPerformance />
+              <p className="text-gray-400 text-sm">Performance</p>
+              <p className="font-semibold">56%</p>
+            </article>
           </div>
-        </section>
+        </div>
+      </section>
     </>
   );
 }
