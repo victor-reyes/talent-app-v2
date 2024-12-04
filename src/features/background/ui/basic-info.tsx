196e1293e9ae501cc3c2bcf81921bdd589372622
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-04 14:46:28 +0100

    change div to section
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Daniel <daniel_danne32@hotmail.com>

--- src/features/background/ui/basic-info.tsx
+++ src/features/background/ui/basic-info.tsx
@@ -1,15 +1,15 @@
 import { HeaderH2 } from "@/components/ui/header/header-h2";
-import { DeveloperAvatar } from "./developer-avatar";
+import { BackgroundAvatar } from "./avatar";
 import { EditInfoForm } from "./form-info";
 
 type Props = { name: string; title: string; bio: string; avatarURL: string };
 
-export function BasicInfo({ name, title, bio, avatarURL }: Props) {
-  const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "true";
+export function BackgroundBasicInfo({ name, title, bio, avatarURL }: Props) {
+  const isFeatureBioEnabled = process.env.NEXT_PUBLIC_FEATURE_BIO === "ON";
   return (
     <>
-      <div className="flex gap-6">
-        <DeveloperAvatar url={avatarURL} />
+      <section className="flex gap-6">
+        <BackgroundAvatar url={avatarURL} />
         <div className="leading-5">
           <p className="uppercase text-sm font-semibold">{title}</p>
           <HeaderH2>{name}</HeaderH2>
@@ -18,7 +18,8 @@ export function BasicInfo({ name, title, bio, avatarURL }: Props) {
           )}
         </div>
         <EditInfoForm />
-      </div>
+      </section>
     </>
-  );}
+  );
+}
 
