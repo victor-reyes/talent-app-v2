2cfd5c62f63f4716f47a0d0ae30a3c48e80ef25b
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-04 17:09:42 +0100

    passing formdata through add background
    Co-authored-by: Víctor <5187756+victor-reyes@users.noreply.github.com>
    Co-authored-by: Odin <odin.thif@appliedtechnology.se>
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- src/features/background/actions.ts
+++ src/features/background/actions.ts
@@ -1,24 +1,9 @@
 "use server";
 
 import { backgroundsService } from "./instance";
-import { BackgroundInsert } from "./schema";
 
 export async function addBackgroundAction(formData: FormData) {
-  const name = formData.get("name") as string;
-  const title = formData.get("title") as string;
-  const bio = formData.get("bio") as string;
-
-  const background: BackgroundInsert = {
-    name,
-    title,
-    bio,
-    languages: [],
-    educations: [],
-    skills: [],
-    links: [],
-  };
-
-  await backgroundsService.add(background);
+  await backgroundsService.add(formData);
 }
 
 export async function updateBackgroundAction(formData: FormData) {
