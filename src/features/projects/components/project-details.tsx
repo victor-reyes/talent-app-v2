b438eaa7f2e74d19d5c009588943a614d8662bf3
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-04 09:58:07 +0100

    updated the the names of the displayed values

--- src/features/projects/components/project-details.tsx
+++ src/features/projects/components/project-details.tsx
@@ -25,13 +25,15 @@ export default async function ProjectDetails({ project }: Props) {
   return (
     <>
       <Link href={`https://github.com/${username}/${repository}`}>
-        <HeaderH3>{title}</HeaderH3>
+        <HeaderH3>{repository}</HeaderH3>
         {/* <FaGithub size={26} color="gray" /> */}
         <section className="flex flex-col justify-center items-center mt-6">
           <article className="flex flex-col items-center mb-4">
             <GiDuration size={28} color="gray" />
             <p className="text-gray-400 text-lg">Duration</p>
-            <p className="font-semibold text-xl">{duration}</p>
+            <p className="font-semibold text-xl">
+              {repositoryInformation.duration}
+            </p>
           </article>
 
           <div className="flex justify-between gap-6">
@@ -39,25 +41,25 @@ export default async function ProjectDetails({ project }: Props) {
               <article className="flex flex-col items-center">
                 <FaCodeCommit />
                 <p className="text-gray-400 text-sm">Commits</p>
-                <p className="font-semibold">{commits}</p>
+                <p className="font-semibold">{repositoryInformation.commits}</p>
               </article>
               <Separator orientation="vertical" />
               <article className="flex flex-col items-center">
                 <GoIssueOpened />
                 <p className="text-gray-400 text-sm">Issues</p>
-                <p className="font-semibold">{issues}</p>
+                <p className="font-semibold">{repositoryInformation.issues}</p>
               </article>
               <Separator orientation="vertical" />
               <article className="flex flex-col items-center">
                 <CgPerformance />
                 <p className="text-gray-400 text-sm">Performance</p>
-                <p className="font-semibold">{performance}%</p>
+                <p className="font-semibold">56%</p>
               </article>
               <Separator orientation="vertical" />
               <article className="flex flex-col items-center">
                 <SiTestcafe />
                 <p className="text-gray-400 text-sm">Tests</p>
-                <p className="font-semibold">{tests}</p>
+                <p className="font-semibold">10</p>
               </article>
             </div>
           </div>
