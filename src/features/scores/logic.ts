e7cfb269b9a371a7b4723fd731dc93d824dd6155
CarlUdo <carl@wbsweden.com>
2024-12-05 12:25:57 +0100

    Make-helper-function-for-calculating-scores
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/logic.ts
+++ src/features/scores/logic.ts
@@ -1,11 +1,26 @@
-export function calculateAverageLevel(communicationLevel: number, planningLevel: number, codingLevel: number) {
+// export function calculateAverageLevel(communicationLevel: number, planningLevel: number, codingLevel: number) {
+
+import { DeveloperScore } from "./types";
 
 
-    const sumLevel = communicationLevel + planningLevel + codingLevel;
+//     const sumLevel = communicationLevel + planningLevel + codingLevel;
 
-    const level = sumLevel / 50;
+//     const level = sumLevel / 50;
 
 
-    return level; 
-}
+//     return level; 
+// }
 
+export const calculateCategoriesScores = (developerScore: DeveloperScore) => {
+    const programmingScore = (developerScore.frontend + developerScore.backend) / 2;
+    const planningScore = (developerScore.management + developerScore.design) / 2;
+    const communicationScore = (developerScore.individualCommunication + developerScore.teamCollaboration) / 2;
+    const averageScore = Math.round((programmingScore + planningScore + communicationScore) / 3);
+    
+    return {
+        programmingScore,
+        planningScore,
+        communicationScore,
+        averageScore
+    };
+};
