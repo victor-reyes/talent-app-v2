7b5c1fa0ae2c6d1a336e533801f0d41e4fbda663
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-05 16:34:52 +0100

    implomented-developerscore
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/ui/score-board.tsx
+++ src/features/scores/ui/score-board.tsx
@@ -4,15 +4,19 @@ import { AverageScore } from "./average-score";
 import { SpiderGraph } from "./spider-graph";
 import { HeaderH2 } from "@/components/ui/header/header-h2";
 import { scoresService } from "../instance";
+import { EditScoreForm } from "./accordion/edit-score-form";
 
 export async function ScoreBoard() {
-  const hardcodedUserId = 1;
+  const hardcodedUserId = 2;
   const developerScore = await scoresService.getDeveloperScoreById(hardcodedUserId);
 
   return (
     <section>
       <Separator className="my-4" />
+      <div className="flex justify-between items-center">
       <HeaderH2>Salt Scoring</HeaderH2>
+      <EditScoreForm developerScore={developerScore} />
+      </div>
       <AverageScore averageScore={developerScore.averageScore} />
       <SpiderGraph developerScore={developerScore} />
       <AccordionBoard developerScore={developerScore} />
