f09448a16af2aa310b544e99a89dfb577788d46e
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-05 14:15:31 +0100

    add project-state component

---
+++ src/features/projects/components/project-state.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+
+import ProjectDetails from "./project-details";
+import { useState } from "react";
+import EditProjectDetails from "./edit-project-details";
+import { Project } from "../types";
+
+type Props = {
+  project: Project;
+};
+export function ProjectState({project}: Props){
+ const [showDetails, setShowDetails] = useState(true)
+ if(!showDetails){
+  return <EditProjectDetails setShowDetails={setShowDetails} project={project}/>;
+ }
+ return (
+   <>
+     <ProjectDetails setShowDetails={setShowDetails} project={project} />
+   </>
+ );
+}
