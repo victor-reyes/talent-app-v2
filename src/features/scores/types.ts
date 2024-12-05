ea7cc5daa18c3ca52a387fa0f351a8057d82fce0
CarlUdo <carl@wbsweden.com>
2024-12-05 12:01:05 +0100

    Update-types
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/features/scores/types.ts
+++ src/features/scores/types.ts
@@ -1,16 +1,22 @@
 export type DeveloperScore = {
+    id: number;
     frontend: number;
     backend: number;
     individualCommunication: number;
     teamCollaboration: number;
     design: number;
     management: number;
+}
+
+export type CompleteDeveloperScore = DeveloperScore & {
     programmingScore: number;
     planningScore: number;
     communicationScore: number;
     averageScore: number;
 }
 
-export type Developer = DeveloperScore & {
-    id: number;
-}
+
+
+// export type Developer = DeveloperScore & {
+//     id: number;
+// }
