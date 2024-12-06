a60f05bcd6fe3b65355816d3271a03500acb7672
Anton Berglund <antonberglund_92@hotmail.com>
2024-12-06 10:41:30 +0100

    added-try-and-catch
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/seed.ts
+++ src/features/scores/seed.ts
@@ -1,15 +1,20 @@
 import { scoresService } from "./instance";
 
 export const seedDeveloperScores = async () => {
-  const newDeveloperScore = {
-    frontend: Math.floor(Math.random() * 101),
-    backend: Math.floor(Math.random() * 101),
-    individualCommunication: Math.floor(Math.random() * 101),
-    teamCollaboration: Math.floor(Math.random() * 101),
-    design: Math.floor(Math.random() * 101),
-    management: Math.floor(Math.random() * 101),
-  };
+    const newDeveloperScore = {
+        frontend: Math.floor(Math.random() * 101),
+        backend: Math.floor(Math.random() * 101),
+        individualCommunication: Math.floor(Math.random() * 101),
+        teamCollaboration: Math.floor(Math.random() * 101),
+        design: Math.floor(Math.random() * 101),
+        management: Math.floor(Math.random() * 101),
+    };
 
-  await scoresService.addDeveloperScores(newDeveloperScore);
-  console.log("Done seeding scores...");
+
+    try {
+        await scoresService.addDeveloperScores(newDeveloperScore);
+    } catch (error) {
+        console.log("Something went wrong when seeding scores " + error);
+    }
+    console.log("Done seeding scores...");
 };
