88479a1a896959b8816a29bb7555a88900799c90
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-06 10:09:47 +0100

    made-zod-exportable
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/zod-validation.ts
+++ src/features/scores/zod-validation.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
-const frontendScore = z.number().min(0).max(100);
-const backendScore = z.number().min(0).max(100);
-const teamCollaborationScore = z.number().min(0).max(100);
-const individualCommunicationScore = z.number().min(0).max(100);
-const designScore = z.number().min(0).max(100);
-const managementScore = z.number().min(0).max(100);
+export const DeveloperScoreSchema = z.object({
+    frontend: z.number().min(0).max(100),
+    backend: z.number().min(0).max(100),
+    teamCollaboration: z.number().min(0).max(100),
+    individualCommunication: z.number().min(0).max(100),
+    design: z.number().min(0).max(100),
+    management: z.number().min(0).max(100),
+});
