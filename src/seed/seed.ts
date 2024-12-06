1a881c070ad9cbe2e43a7546400475eba9f0d761
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-06 14:17:26 +0100

    fixed
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,10 +1,10 @@
 import { seed } from "@/features/background/seed/seed";
-import { seedDeveloperScores } from "@/features/scores/seed";
+import { seedScores } from "@/features/scores/seed";
 import { seedProjects } from "@/features/projects/db/seed";
 
 (async () => {
   console.log("Start seeding...");
-  await Promise.all([seedDeveloperScores() /* seed() */]);
+  await Promise.all([seedScores() /* seed() */]);
   seed();
   seedProjects();
   console.log("Done seeding...");
