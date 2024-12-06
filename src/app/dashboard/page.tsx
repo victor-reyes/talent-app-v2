bb550902258881dfa938ae028532c2afe6dddcef
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-06 14:39:34 +0100

    Create dashboard page with 3 tabs developers, customers and core team

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function Page() {
+  return (
+    <div className="container mx-auto flex flex-col justify-center items-center">
+      <h1>Dashboard</h1>
+      <Tabs defaultValue="developers" className="w-[400px] flex flex-col">
+        <TabsList className="space-x-3 w-fit mx-auto">
+          <TabsTrigger value="developers">Developers</TabsTrigger>
+          <TabsTrigger value="core">Core</TabsTrigger>
+          <TabsTrigger value="customers">Customers</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="developers">List with developers.</TabsContent>
+        <TabsContent value="core">List with core team.</TabsContent>
+        <TabsContent value="customers">List with customers</TabsContent>
+      </Tabs>
+    </div>
+  );
+}
