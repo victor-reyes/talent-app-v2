7cfd75d725ad67aaf4bfd5b51e4817d7c1e5a27f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-09 16:02:35 +0100

    service function to get project image

--- src/features/projects/service.ts
+++ src/features/projects/service.ts
@@ -1,11 +1,12 @@
 import { Db } from "@/db";
 import { createRepository } from "./repository";
 import { projectInsert } from "./db";
-// import { createClient } from "./api/api";
+import { createClient } from "./api/api";
 import { ProjectData, UpdatedProject } from "./types";
 
 export function createService(db: Db) {
   const reps = createRepository(db);
+  const client = createClient();
   return {
     getAll: async () => {
       return await reps.getAll();
@@ -17,9 +18,9 @@ export function createService(db: Db) {
       description,
       performance,
     }: ProjectData) => {
-      //const client = createClient();
       //const commits = await client.getTotalOfCommits(username, title);
       //const issuesArr = await client.getAllIssues(username, title);
+      // const images = "image.png";
       const commits = "120";
       const issues = "52";
 
@@ -37,6 +38,9 @@ export function createService(db: Db) {
     },
     update: async (updatedProject: UpdatedProject) => {
       await reps.update(updatedProject);
+    },
+    getImage: async (username: string, title: string, image: string) => {
+      return await client.getImage(username, title, image);
     },
   };
 }
