851e1b45ccfd8fab515f061a15de3105cc4fae49
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 14:12:19 +0100

    made-schema-for-assigmnets
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/schema.ts
+++ src/features/scores/schema.ts
@@ -1,4 +1,5 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar} from "drizzle-orm/pg-core";
+
 
 export const scoresTable = pgTable("scores", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -8,4 +9,13 @@ export const scoresTable = pgTable("scores", {
   teamCollaboration: integer().notNull(),
   design: integer().notNull(),
   management: integer().notNull(),
+});
+
+export const assignmentTable = pgTable("score_assigments",{
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  comment: varchar(),
+  score: integer(),
+  tags: varchar().array(),
+  //userId: integer().notNull()
 });
