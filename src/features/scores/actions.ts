703582a3c3df17cb1a04bcbb2c06fd35e24c62da
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 11:17:35 +0100

    add-more-functionality-to-actions.ts
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/actions.ts
+++ src/features/scores/actions.ts
@@ -2,17 +2,18 @@
 import { scoresService } from "./instance";
 
 export async function updateScoresAction(formData: FormData) {
+  const title = formData.get("title") as string;
+  const comment = formData.get("comment") as string;
+  const score = formData.get("score") as string;
   const individualCommunication = formData.get(
     "individualCommunication"
   ) as string;
-  const title = formData.get("title") as string;
-  const comment = formData.get("comment") as string;
   const teamCollaboration = formData.get("teamCollaboration") as string;
   const management = formData.get("management") as string;
   const design = formData.get("design") as string;
   const backend = formData.get("backend") as string;
   const frontend = formData.get("frontend") as string;
-console.log(comment)
+console.log(teamCollaboration, management)
   const newScores = {
     frontend: Number(frontend),
     backend: Number(backend),
