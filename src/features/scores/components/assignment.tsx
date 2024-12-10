a6b5d5fed190bf717d7ee98e7550839f288f99e2
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 15:02:31 +0100

    assignemnt-types-and-javascript-for-title-tags-and-comment
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/assignment.tsx
+++ src/features/scores/components/assignment.tsx
@@ -1,28 +1,35 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components";
 import { ProgressRing } from "./progress-ring";
+import { Assignment as AssignmentType } from "../types";
 
-  
-  
-  export function Assignment() {
-    return (
-      <Accordion type="single" collapsible className="mx-auto max-w-96">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            <ProgressRing percentage={100} />
-            Title of assignemnt
-          </AccordionTrigger>
-          <AccordionContent>
-            <div className="flex flex-col mr-2 pl-8">
-              <span className="flex flex-row gap-4 pb-2 items-center">
-                Comments
-              </span>
-              <span className="flex flex-row gap-4 pb-2 items-center">
-                Tags
-              </span>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    );
-  }
-  
+type Props = {
+  assignment: AssignmentType;
+};
+
+export function Assignment({ assignment }: Props) {
+  return (
+    <Accordion type="single" collapsible className="mx-auto max-w-96">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>
+          <ProgressRing percentage={100} />
+          {assignment.title}
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col mr-2 pl-8">
+            <span className="flex flex-row gap-4 pb-2 items-center">
+              {assignment.comment}
+            </span>
+            <span className="flex flex-row gap-4 pb-2 items-center">
+              {assignment.tags}
+            </span>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
