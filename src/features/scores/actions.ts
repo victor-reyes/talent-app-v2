fd2b432b5b9eba25e795620452c6b71e8ef3ff95
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 12:28:45 +0100

    created-action-for-add-assignment
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/actions.ts
+++ src/features/scores/actions.ts
@@ -1,27 +1,39 @@
 "use server";
 import { scoresService } from "./instance";
 
-export async function updateScoresAction(formData: FormData) {
+export async function addAssigmentAction(formData: FormData) {
   const title = formData.get("title") as string;
   const comment = formData.get("comment") as string;
   const score = formData.get("score") as string;
+  const tags: string[] = [];
   const individualCommunication = formData.get(
     "individualCommunication"
   ) as string;
+  if(individualCommunication) tags.push("individualCommunication")
+
   const teamCollaboration = formData.get("teamCollaboration") as string;
+  if(teamCollaboration) tags.push("teamCollaboration")
+
   const management = formData.get("management") as string;
+  if(management) tags.push("management")
+
   const design = formData.get("design") as string;
+  if(design) tags.push("design")
+
   const backend = formData.get("backend") as string;
+  if(backend) tags.push("backend")
+
   const frontend = formData.get("frontend") as string;
-console.log(teamCollaboration, management)
-  const newScores = {
-    frontend: Number(frontend),
-    backend: Number(backend),
-    individualCommunication: Number(individualCommunication),
-    teamCollaboration: Number(teamCollaboration),
-    design: Number(design),
-    management: Number(management),
+  if(frontend) tags.push("frontend")
+
+
+  const newAssignment = {
+    userId: 1,
+    comment,
+    score,
+    title,
+    tags,
   };
 
-  scoresService.updateScores(2, newScores);
+  scoresService.addAssignment( newAssignment );
 }
