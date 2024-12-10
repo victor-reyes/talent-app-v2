3007344823edce6fad2b126b52cc52e468235639
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 16:01:21 +0100

    imports-porbs-averagescore
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/average-score.tsx
+++ src/features/scores/components/average-score.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { LevelRectangle } from "./level-rectangle";
+import { Assignment as AssignmentType } from "../types";
+import { calculateAverageAssignmentsScore } from "../logic";
+
+
 
 type Props = {
-  averageScore: number;
-};
+  assignments: AssignmentType[]
+}
+export function AverageScore({ assignments }: Props) { 
+const averageScore = calculateAverageAssignmentsScore(assignments)
 
-export function AverageScore({ averageScore }: Props) {
   return <LevelRectangle percentage={averageScore} />;
 }
