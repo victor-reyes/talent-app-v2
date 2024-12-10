1b7cd3023bd955a26ddc0894eb71162c2d10eb67
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-12-10 14:07:54 +0100

    feat(dashboard): add seeding

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,9 +1,15 @@
 import { backgroundsSeed } from "@/features/backgrounds/seed/seed";
 import { seedScores } from "@/features/scores/seed";
 import { seedProjects } from "@/features/projects/seed";
+import { seedIdentities } from "@/features/dashboard/seed";
 
 (async () => {
   console.log("Start seeding...");
-  await Promise.all([seedScores(), backgroundsSeed(), seedProjects()]);
+  await Promise.all([
+    seedScores(),
+    backgroundsSeed(),
+    seedProjects(),
+    seedIdentities(),
+  ]);
   console.log("Done seeding...");
 })();
