8ab54589ccf77952afb3b709e28905fb95d5c8f8
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-10 12:17:59 +0100

    create get all developers, add user and developer functions inside dashboard service

--- src/features/dashboard/service.ts
+++ src/features/dashboard/service.ts
@@ -1,13 +1,24 @@
 import { hasAccess } from "./privileges";
 import { Repository } from "./repository";
+import { DeveloperInsert, UserInsert } from "./schema";
 
 export function createService(repository: Repository) {
   return {
     async getUserById(id: number) {
       return hasAccess() ? await repository.getUserById(id) : "Access denied";
     },
-    async getAllDevelopers(){
-      return hasAccess() ? await repository.getAllDevelopers() : "Access denied";
-    }
+    async getAllDevelopers() {
+      return hasAccess()
+        ? await repository.getAllDevelopers()
+        : "Access denied";
+    },
+    async addUser(user: UserInsert) {
+      return hasAccess() ? await repository.addUser(user) : "Access denied";
+    },
+    async addDeveloper(developer: DeveloperInsert) {
+      return hasAccess()
+        ? await repository.addDeveloper(developer)
+        : "Access denied";
+    },
   };
 }
