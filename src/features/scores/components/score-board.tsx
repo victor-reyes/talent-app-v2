ec132116bc49164e0be249d7c5897260373a6002
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 12:30:45 +0100

    added-to-app-page
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/score-board.tsx
+++ src/features/scores/components/score-board.tsx
@@ -5,6 +5,7 @@ import { H2 } from "@/components/ui/header/header-h2";
 import { scoresService } from "../instance";
 import { CategoryGroups } from "./accordion";
 import { AddAssignment } from "./add-assignment";
+import { Assignments } from "./accordion/assignments";
 
 export async function ScoreBoard() {
   const hardcodedUserId = 1;
@@ -13,12 +14,13 @@ export async function ScoreBoard() {
 
   return (
     <section className="min-w-72">
-      <AddAssignment scores={scores}/>
       <Separator className="my-4" />
       <H2>Salt Scoring</H2>
       <AverageScore averageScore={scores.averageScore} />
       <SpiderGraph scores={scores} />
-      <CategoryGroups scores={scores} />
+      <AddAssignment/>
+      <Assignments/>
+      <CategoryGroups scores={scores}/>
     </section>
   );
 }
