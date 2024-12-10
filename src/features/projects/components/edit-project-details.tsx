459d13e404acd2a2c37e49a5a2458591ce161263
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-10 10:14:09 +0100

    delete inline parts from form

--- src/features/projects/components/edit-project-details.tsx
+++ src/features/projects/components/edit-project-details.tsx
@@ -84,11 +84,7 @@ export default function EditProjectDetails({ project, setShowDetails }: Props) {
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 max-w-3xl mx-auto py-10"
         >
-          <div className="flex justify-between items-baseline">
-            <div className="flex flex-col ">
-              <div className="ml-2  opacity-80 hover:opacity-100">
                 <FormItem>
                   <FormLabel>Title</FormLabel>
                   <FormControl>
@@ -101,41 +97,6 @@ export default function EditProjectDetails({ project, setShowDetails }: Props) {
                     {form.formState.errors.title?.message}
                   </FormMessage>
                 </FormItem>
-                <p className="text-xs text-gray-400 flex items-center gap-2">
-                  <FaGithub size={16} color="gray" />
-                  Last commit {"Today"}
-                </p>
-              </div>
-            </div>
-            <button onClick={toggleEdit}>
-              <X className="mr-4 " size={16} />
-            </button>
-          </div>
-          <section className="flex justify-between items-start mt-2 gap-2">
-            <Image
-              src="/restaurant1.png"
-              width={250}
-              height={240}
-              alt=""
-              className="m-2 object-cover rounded-lg"
-            />
-            <div className="flex flex-col items-center justify-between gap-4 mt-2 mr-2">
-              <article className="flex flex-col items-center my-2">
-                <p className="text-gray-400 text-sm">Commits</p>
-                <p className="font-semibold">{project.commits}</p>
-              </article>
-              <Separator orientation="horizontal" />
-              <article className="flex flex-col items-center my-2">
-                <p className="text-gray-400 text-sm">Issues</p>
-                <p className="font-semibold">{project.issues}</p>
-              </article>
-              <Separator orientation="horizontal" />
-              <article className="flex flex-col items-center my-2">
-                <p className="text-gray-400 text-sm">Performance</p>
-                <p className="font-semibold">56%</p>
-              </article>
-            </div>
-          </section>
           <FormItem>
             <FormLabel>Description</FormLabel>
             <FormControl>
