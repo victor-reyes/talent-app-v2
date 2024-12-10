27e732a61da92701543d8cf12846898ba1f99cfe
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-12-10 12:32:03 +0100

    fixed-edit-fomr-removed-imports
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/features/scores/components/accordion/edit-score-form.tsx
+++ src/features/scores/components/accordion/edit-score-form.tsx
@@ -10,9 +10,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { updateScoresAction } from "../../actions";
+import { addAssigmentAction } from "../../actions";
 import { useState } from "react";
 import { Scores } from "../../types";
+import { AddAssignment } from "../add-assignment";
 
 type Props = {
   scores: Scores;
@@ -43,80 +44,7 @@ export function EditScoreForm({ scores: scores }: Props) {
             done.
           </DialogDescription>
         </DialogHeader>
-        <form action={updateScoresAction}>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="individualCommunication" className="text-right">
-                Individual Communication
-              </label>
-              <input
-                value={individualComunicationValue}
-                onChange={(e) =>
-                  setIndividualComunicationValue(Number(e.target.value))
-                }
-                name="individualCommunication"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="teamCollaboration" className="text-right">
-                Team Collaboration
-              </label>
-              <input
-                value={teamCollaborationValue}
-                onChange={(e) =>
-                  setTeamCollaborationValue(Number(e.target.value))
-                }
-                name="teamCollaboration"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="management" className="text-right">
-                Management
-              </label>
-              <input
-                value={managementValue}
-                onChange={(e) => setManagementValue(Number(e.target.value))}
-                name="management"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="design" className="text-right">
-                Design
-              </label>
-              <input
-                value={designValue}
-                onChange={(e) => setDesignValue(Number(e.target.value))}
-                name="design"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="backend" className="text-right">
-                Backend
-              </label>
-              <input
-                value={backendValue}
-                onChange={(e) => setBackendValue(Number(e.target.value))}
-                name="backend"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="frontend" className="text-right">
-                Frontend
-              </label>
-              <input
-                value={frontendValue}
-                onChange={(e) => setFrontendValue(Number(e.target.value))}
-                name="frontend"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-
+        <form action={addAssigmentAction}>
           <DialogFooter>
             <Button type="submit">Save changes</Button>
           </DialogFooter>
